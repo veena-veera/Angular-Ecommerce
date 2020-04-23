0f679d6d73211c6e935c8c7ced2c081533901fe2
veena-veera <veena.rddy4@gmail.com>
2020-04-23 12:37:52 -0400

    login and register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewChild  } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('f') loginForm: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
